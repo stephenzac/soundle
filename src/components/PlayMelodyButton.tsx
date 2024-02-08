c96e93e50de9fc009cb5a7118486d587a873e458
Stephen Zacarias <stephenzacarias2@gmail.com>
2024-02-08 15:55:19 -0800

    Fix melody spam

--- src/components/PlayMelodyButton.tsx
+++ src/components/PlayMelodyButton.tsx
@@ -1,14 +1,26 @@
+import { useState } from "react";
 import { PlayMelody } from "../GameNotes";
 import { useBoardContext } from "../contexts/BoardContext";
 
+//TODO: Only play melody once per guess?
+
+const MELODY_WAIT_TIME = 2600;
+
 const PlayMelodyButton = () => {
   const { melody } = useBoardContext();
+  const [clicked, setClicked] = useState(false);
 
   return (
     <div
       className="round-button"
       onClick={() => {
-        PlayMelody(melody);
+        if (!clicked) {
+          setClicked(true);
+          PlayMelody(melody);
+          setTimeout(() => {
+            setClicked(false);
+          }, MELODY_WAIT_TIME);
+        }
       }}
     >
       ♫
