f451ff235d5e63fb332b428399089ab3d12bc9be
Stephen Zacarias <stephenzacarias2@gmail.com>
2024-02-09 13:22:44 -0800

    Change logo and name

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,10 @@ import MainGame from "./components/MainGame";
 import { BoardContextProvider } from "./contexts/BoardContext";
 
 const App = () => {
-  // TODO:Change title
   return (
     <div className="bg-gray-800 h-screen flex flex-col">
       <h1 className="text-center text-4xl text-slate-100 font-bold w-full pt-2 pb-4">
-        Melodle Clone
+        Soundle
       </h1>
       <BoardContextProvider>
         <MainGame />
